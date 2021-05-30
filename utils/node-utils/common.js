310e7ae641666df2591c20e48016c20ea0cb7eb2
Sadegh Teymouri <sadeghte@gmail.com>
2021-05-30 13:20:45 +0430

    cid version from 0 to 1

--- utils/node-utils/common.js
+++ utils/node-utils/common.js
@@ -1,14 +1,23 @@
 const CID = require('cids')
-const crypto = require('../crypto')
 const multihashing = require('multihashing-async')
 
 async function strToCID(str) {
   const bytes = new TextEncoder('utf8').encode(`${str}`)
 
   const hash = await multihashing(bytes, 'sha2-256')
-  return new CID(0, 'dag-pb', hash)
+  return new CID(1, 'dag-pb', hash, 'base16')
+}
+
+// https://github.com/multiformats/multibase#multibase-table
+function cid2hex(cid){
+  return cid.toString().substr(1);
+}
+function hex2cid(hex){
+  return new CID('f' + hex.toLowerCase());
 }
 
 module.exports = {
   strToCID,
+  cid2hex,
+  hex2cid
 }
