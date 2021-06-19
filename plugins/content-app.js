4299660cbea1070c31072fb97966186ff013978c
Sadegh Teymouri <sadeghte@gmail.com>
2021-06-19 14:21:40 +0430

    verify method added to content app

--- plugins/content-app.js
+++ plugins/content-app.js
@@ -1,64 +1,69 @@
 const BaseApp = require('./base/base-app-plugin')
+const Content = require('../gateway/models/Content')
 const CID = require('cids')
-const NodeUtils = require('../utils/node-utils')
-const fs = require('fs')
-const chokidar = require('chokidar');
 const all = require('it-all')
 const {remoteApp, remoteMethod, gatewayMethod} = require('./base/app-decorators')
 
 @remoteApp
 class ContentApp extends BaseApp {
   APP_NAME = 'content'
-  watcher = null;
 
-  async onNewFileAdd(path){
-    // console.log('Request File', path, 'has been added')
-    let cidStr = new CID(path.split('.')[0]);
-    await this.muon.libp2p.contentRouting.provide(cidStr)
-  }
-
-  onFileRemove(path){
-    // console.log('Request File', path, 'has been removed');
-  }
-
-  async onRequestSigned(request){
-    let content = JSON.stringify(request);
-    let cid = await NodeUtils.stock.createCID(request)
-    fs.writeFileSync(`./data/${cid.toString()}.req`, content)
+  async onGatewayConfirmed(response){
+    let content = await Content.create(response)
+    await content.save();
+    response['cid'] = content.cid;
   }
 
   async onStart() {
-    this.muon.getPlugin('stock-plugin').on('request-signed', this.onRequestSigned.bind(this))
+    this.muon.getPlugin('gateway-interface').on('confirmed', this.onGatewayConfirmed.bind(this))
 
-    this.watcher = chokidar.watch('*.req', {
-      // ignored: /^\./,
-      ignoreInitial: false,
-      persistent: true,
-      depth: 2,
-      cwd: `./data`
-    });
-    this.watcher
-      .on('add', this.onNewFileAdd.bind(this))
-      .on('unlink', this.onFileRemove.bind(this))
+    let contents = await Content.find({});
+    for(let i in contents){
+      let {cid} = contents[i]
+      await this.muon.libp2p.contentRouting.provide(new CID(cid))
+    }
+  }
 
+  async getContent(cid){
+    return this.responseToGatewayRequestData({cid})
+  }
+
+  @gatewayMethod('verify')
+  async verifyContent(data){
+    let {cid} = data;
+    let content = await this.muon.getPlugin('content').getContent(cid)
+    let verifyPlugin = this.muon.getPlugin('content-verify');
+    if(content){
+      let [verified, description, expectedResult, actualResult] = await verifyPlugin.verifyContent(content, cid)
+      return {
+        verified,
+        description,
+        expectedResult,
+        actualResult,
+        data
+      }
+    }
+    else{
+      return {
+        verified: false,
+        description: 'no content',
+        data,
+      }
+    }
   }
 
   @gatewayMethod('get_content')
   async responseToGatewayRequestData(data){
-    let filePath = `./data/${data.cid}.req`
-    if(fs.existsSync(filePath, 'utf8')){
-      let content = fs.readFileSync(filePath)
-      let requestData = JSON.parse(content)
-      return requestData
-    }
-    else{
+    let content = await Content.findOne({cid: data.cid});
+    if(content){
+      return content.content;
+    }else{
       let cid = new CID(data.cid);
       let providers = await all(this.muon.libp2p.contentRouting.findProviders(cid, {timeout: 5000}))
       for(let provider of providers){
         if(provider.id.toB58String() !== process.env.PEER_ID){
           let peer = await this.muon.libp2p.peerRouting.findPeer(provider.id);
           let request = await this.remoteCall(peer, 'get_content', data)
-          // let request = await NodeUtils.getRequestInfo(data.id)
           return request
         }
       }
@@ -68,14 +73,11 @@ class ContentApp extends BaseApp {
 
   @remoteMethod('get_content')
   async responseToRemoteRequestData(data){
-    let filePath = `./data/${data.cid}.req`
-    if(fs.existsSync(filePath, 'utf8')){
-      let content = fs.readFileSync(filePath)
-      return JSON.parse(content)
-    }
-    else{
-      return null
-    }
+    let content = await Content.findOne({cid: data.cid});
+    if(content)
+      return content.content
+    else
+      return null;
   }
 }
 
