833b21b4c8e985ec35e57b5acc19d8aae600b3e0
Sadegh Teymouri <sadeghte@gmail.com>
2021-06-27 09:49:16 +0430

    finnhub get price of timestamp

--- gateway/sources/finnhub/index.js
+++ gateway/sources/finnhub/index.js
@@ -2,11 +2,11 @@ const Api = require('./api');
 require('./websocket')
 const PriceCache = require('./price-cache')
 
-async function getSymbolPrice(symbol) {
+async function getSymbolPrice(symbol, timestamp) {
   let result = await PriceCache.getSymbolPrice(symbol)
-  if(!result){
+  if(!result || !!timestamp){
     // console.log('Price not exist in redis')
-    result = await Api.getStockPrice(symbol)
+    result = await Api.getStockPrice(symbol, timestamp)
     // console.log('price from api', result)
   }
   return result
