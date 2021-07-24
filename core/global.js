a0f44f9a35fdd390beb7a7acbc84c4e05667c513
Sadegh Teymouri <sadeghte@gmail.com>
2021-07-24 19:59:58 +0430

    add timeout (waiting for a while) to global

--- core/global.js
+++ core/global.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 const Web3 = require('web3')
 const web3Instance = new Web3;
 const {toBaseUnit} = require('../utils/crypto')
+const {timeout} = require('../utils/helpers')
 const util = require('ethereumjs-util');
 const ethSigUtil = require('eth-sig-util')
 const {read : ethRead, call: ethCall} = require('../utils/node-utils/eth')
@@ -13,6 +14,7 @@ function soliditySha3(params){
 global.MuonAppUtils = {
   axios,
   Web3,
+  timeout,
   BN: Web3.utils.BN,
   ethRead,
   ethCall,
