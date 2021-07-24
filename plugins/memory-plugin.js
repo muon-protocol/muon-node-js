a908a5c8ecdf3603e7ad61f080e60f2f6b4ce43e
Sadegh Teymouri <sadeghte@gmail.com>
2021-07-24 20:06:04 +0430

    memory plugin update

--- plugins/memory-plugin.js
+++ plugins/memory-plugin.js
@@ -2,6 +2,7 @@ const BasePlugin = require('./base/base-plugin')
 const uint8ArrayFromString = require('uint8arrays/from-string')
 const uint8ArrayToString = require('uint8arrays/to-string')
 const crypto = require('../utils/crypto')
+const {getTimestamp} = require('../utils/helpers')
 const Memory = require('../gateway/models/Memory')
 
 class MemoryPlugin extends BasePlugin {
@@ -38,6 +39,9 @@ class MemoryPlugin extends BasePlugin {
         if(this.checkSignature(data.memWrite)){
           this.storeMemWrite(data.memWrite);
         }
+        else{
+          console.log('memWrite signature mismatch', data.memWrite)
+        }
       }
     } catch (e) {
       console.error(e)
@@ -45,32 +49,72 @@ class MemoryPlugin extends BasePlugin {
   }
 
   checkSignature(memWrite){
-    let {app, timestamp, ttl, nSign, data, signatures} = memWrite;
-    let hash = crypto.soliditySha3([
-      {type: 'string', value: app},
+    let {signatures} = memWrite;
+    let hash = this.hashMemWrite(memWrite)
+    if(hash !== memWrite.hash) {
+      console.log('hash mismatch', [hash, memWrite.hash])
+      return false
+    }
+    let allowedList = this.muon.getNodesWalletList();
+    let sigOwners = signatures.map(sig => crypto.recover(hash, sig));
+    for(const address of sigOwners){
+      if(allowedList.indexOf(addr => (addr.toLowerCase()===address.toLowerCase())) < 0) {
+        return false
+      }
+    }
+    return true;
+  }
+
+  hashMemWrite(memWrite) {
+    let {type, owner, timestamp, ttl, nSign, data} = memWrite;
+    return crypto.soliditySha3([
+      {type: 'string', value: type},
+      {type: 'string', value: owner},
       {type: 'uint256', value: timestamp},
       {type: 'uint256', value: ttl},
       {type: 'uint256', value: nSign},
       ... data.map(({type, value}) => ({type, value})),
     ])
-    if(hash !== memWrite.hash)
-      return false
-    // let sigOwners = signatures.map(sig => crypto.recover(hash, sig));
-    return true;
   }
 
-  async writeRequestMem(request) {
+  async writeAppMem(request) {
     if(!request.data.memWrite)
       return;
 
     let {timestamp, ttl, nSign, data, hash} = request.data.memWrite;
     let signatures = request.signatures.map(sign => sign.memWriteSignature)
     let memWrite = {
-      app: request.app,
+      type: Memory.types.App,
+      owner: request.app,
       timestamp,
       ttl,
       nSign,
       data,
+      hash,
+      signatures,
+    }
+    this.storeMemWrite(memWrite);
+    this.broadcastWrite(memWrite);
+  }
+
+  async writeNodeMem(memory) {
+    let {ttl, data} = memory;
+    let nSign=1,
+      timestamp=getTimestamp();
+
+    let memWrite = {
+      type: Memory.types.Node,
+      owner: process.env.SIGN_WALLET_ADDRESS,
+      timestamp,
+      ttl,
+      nSign,
+      data,
+    }
+    let hash = this.hashMemWrite(memWrite)
+    let signatures = [crypto.sign(hash)]
+
+    memWrite = {
+      ...memWrite,
       hash,
       signatures,
     }
@@ -91,12 +135,42 @@ class MemoryPlugin extends BasePlugin {
     mem.save();
   }
 
-  async readAppMem(app, query) {
-    return Memory.findOne({...query, app})
+  async readAppMem(app, query, options={}) {
+    let {
+      multi=false,
+      distinct=null,
+    } = options;
+
+    if(!!distinct){
+      return Memory.distinct(distinct, {...query, type: Memory.types.App, owner: app})
+    }
+    else{
+      if(multi){
+        return Memory.find({...query, type: Memory.types.App, owner: app})
+      }
+      else{
+        return Memory.findOne({...query, type: Memory.types.App, owner: app})
+      }
+    }
   }
 
-  async readAppMemMulti(app, query) {
-    return Memory.find({...query, app})
+  async readNodeMem(query, options={}) {
+    let {
+      multi=false,
+      distinct=null,
+    } = options
+
+    if(!!distinct) {
+      return Memory.distinct(distinct, query)
+    }
+    else{
+      if(multi) {
+        return Memory.find({...query, type: Memory.types.Node})
+      }
+      else {
+        return Memory.findOne({...query, type: Memory.types.Node})
+      }
+    }
   }
 }
 
