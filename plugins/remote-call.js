29a63bdcf7c271c00de2c047df5356f1d031b617
Sadegh Teymouri <sadeghte@gmail.com>
2021-10-13 15:49:38 +0330

    remote-call timeout. memory clean when call fulfilled.

--- plugins/remote-call.js
+++ plugins/remote-call.js
@@ -1,13 +1,36 @@
 const BasePlugin = require('./base/base-plugin.js')
 const pipe = require('it-pipe')
 const {newCallId} = require('../utils/helpers')
-const uint8ArrayToString = require('uint8arrays/to-string')
+const TimeoutPromise = require('../core/timeout-promise')
 const crypto = require('../utils/crypto')
+const NodeCache = require('node-cache');
+
+const callCache = new NodeCache({
+  stdTTL: 6*60, // Keep call in memory for 6 minutes
+  // stdTTL: 10, // Keep call in memory for 10 seconds
+  // /**
+  //  * (default: 600)
+  //  * The period in seconds, as a number, used for the automatic delete check interval.
+  //  * 0 = no periodic check.
+  //  */
+  // checkperiod: 5,
+  useClones: false,
+});
+
+// callCache.on( "set", function( key, value ){
+//   console.log(`adding remote-call [${key}]`)
+// });
+// callCache.on( "del", function( key, value ){
+  // console.log(`deleting remote-call [${key}] isFulFilled: ${value.resultPromise.isFulfilled}`, value.method)
+  // let {resultPromise} = value
+  // if(!resultPromise.isFulfilled){
+  //   resultPromise.reject({message: 'remote-call timed out.'})
+  // }
+// });
 
 const PROTOCOL = '/muon/remote-call/1.0.0'
 
 class RemoteCall extends BasePlugin {
-  _calls = {}
 
   async onStart() {
     this.muon.libp2p.handle(PROTOCOL, this.handler.bind(this))
@@ -56,12 +79,18 @@ class RemoteCall extends BasePlugin {
       }
       else if('responseId' in data){
         let {responseId, response=undefined, error=undefined} = data;
-        let remoteResult = this._calls[responseId]
-        if(!error)
-          remoteResult.resolve(response)
-        else {
-          console.log('remote side error', error);
-          remoteResult.reject(error)
+        // let remoteResult = this._calls[responseId]
+        let {resultPromise=null} = callCache.get(responseId);
+        if(resultPromise) {
+          if (!error)
+            resultPromise.resolve(response)
+          else {
+            console.log('remote side error', error);
+            resultPromise.reject(error)
+          }
+        }
+        else{
+          // TODO: what to do? it may timed out.
         }
       }
     }catch (e) {
@@ -110,12 +139,12 @@ class RemoteCall extends BasePlugin {
     return this.muon.libp2p.dialProtocol(peer.id, [PROTOCOL])
   }
 
-  call(peer, method, params){
+  call(peer, method, params, options){
     return this.getPeerCallStream(peer)
       .then(({stream}) => {
         if(!stream)
           console.log('no stream call ... ')
-        return this.callStream(stream, peer, method, params)
+        return this.callStream(stream, peer, method, params, options)
       })
       .catch(e => {
         console.error(`RemoteCall.call(peer, '${method}', params)`, e)
@@ -123,23 +152,26 @@ class RemoteCall extends BasePlugin {
       })
   }
 
-  callStream(stream, peer, method, params){
+  callStream(stream, peer, method, params, options){
+    options = {
+      timeout: 5000,
+      timeoutMessage: "remoteCall timeout!",
+      ...(!!options ? options : {})
+    };
+
     let callId = newCallId();
     this.send({callId, method, params}, stream, peer)
-    let remoteResult = new RemoteResult()
-    this._calls[callId] = remoteResult;
-    // TODO: clear this._calls[callID] when remoteResult fullFilled.
-    return remoteResult.promise
+    let resultPromise = new TimeoutPromise(options.timeout, options.timeoutMessage)
+    // this._calls[callId] = remoteResult;
+    callCache.set(callId, {
+      method,
+      params,
+      peer,
+      options,
+      resultPromise
+    });
+    return resultPromise.promise;
   }
-}
-
-// TODO: replace with TimeoutPromise
-function RemoteResult() {
-  var self = this;
-  this.promise = new Promise(function(resolve, reject) {
-    self.reject = reject
-    self.resolve = resolve
-  })
 }
 
 module.exports = RemoteCall;
