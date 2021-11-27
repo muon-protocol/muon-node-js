2393d787c673cd9758e93d2cdf62fa2279deb8d6
Zahra Namazi <namazi.q@gmail.com>
2021-11-27 09:08:46 +0330

    initial prealeFear app

---
+++ apps/general/fear_presale.js
@@ -0,0 +1,34 @@
+module.exports = {
+  APP_NAME: 'fear_presale',
+  APP_ID: 6,
+
+  onRequest: async function (request) {
+    let {
+      method,
+      data: { params }
+    } = request
+
+    switch (method) {
+      case 'deposit':
+        break
+
+      default:
+        throw { message: `Unknown method ${params}` }
+    }
+  },
+
+  hashRequestResut: function (request, result) {
+    let {
+      method,
+      data: { params }
+    } = request
+
+    switch (method) {
+      case 'deposit':
+        break
+
+      default:
+        return null
+    }
+  }
+}
