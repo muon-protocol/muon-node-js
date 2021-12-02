50bba779d47c647ec32471ce98fa979daea741ac
Zahra Namazi <namazi.q@gmail.com>
2021-12-02 13:49:35 +0330

    add deus_bridge app

---
+++ apps/general/deus_bridge.js
@@ -0,0 +1,102 @@
+const { ethCall, soliditySha3 } = MuonAppUtils
+
+const ABI_getTx = [
+  {
+    inputs: [{ internalType: 'uint256', name: '_txId', type: 'uint256' }],
+    name: 'getTx',
+    outputs: [
+      { internalType: 'uint256', name: 'txId', type: 'uint256' },
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      { internalType: 'uint256', name: 'fromChain', type: 'uint256' },
+      { internalType: 'uint256', name: 'toChain', type: 'uint256' },
+      { internalType: 'address', name: 'user', type: 'address' },
+      { internalType: 'uint256', name: 'txBlockNo', type: 'uint256' }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  }
+]
+
+const confirmationBlock = {
+  polygon: 256,
+  mumbai: 256,
+  eth: 24,
+  ropsten: 24,
+  rinkeby: 24,
+  bsc: 30,
+  bsctest: 30,
+  Avax: 24,
+  ftm: 6,
+  ftmtest: 6
+}
+
+module.exports = {
+  APP_NAME: 'deus_bridge',
+  APP_ID: 7,
+
+  onRequest: async function (request) {
+    let {
+      method,
+      data: { params }
+    } = request
+    switch (method) {
+      case 'claim':
+        let { depositAddress, depositTxId, depositNetwork } = params
+        if (!depositAddress) throw { message: 'Invalid contarct address' }
+        if (!depositTxId) throw { message: 'Invalid depositTxId' }
+        if (!depositNetwork) throw { message: 'Invalid network' }
+
+        let {
+          txId,
+          tokenId,
+          amount,
+          fromChain,
+          toChain,
+          user,
+          txBlockNo,
+          currentBlockNo
+        } = await ethCall(
+          depositAddress,
+          'getTransaction',
+          [depositTxId],
+          ABI_getTx,
+          depositNetwork
+        )
+
+        if (currentBlockNo < txBlockNo + confirmationBlock[depositNetwork])
+          throw { message: 'Bridge: confirmationTime is not finished yet' }
+
+        return { txId, tokenId, amount, fromChain, toChain, user }
+
+      default:
+        throw { message: `Unknown method ${params}` }
+    }
+  },
+
+  hashRequestResult: function (request, result) {
+    let {
+      method,
+      data: { params }
+    } = request
+    switch (method) {
+      case 'claim':
+        let { depositAddress } = params
+        let { txId, tokenId, amount, fromChain, toChain, user } = result
+
+        return soliditySha3([
+          { type: 'address', value: depositAddress },
+          { type: 'uint256', value: txId },
+          { type: 'uint256', value: tokenId },
+          { type: 'uint256', value: amount },
+          { type: 'uint256', value: fromChain },
+          { type: 'uint256', value: toChain },
+          { type: 'address', value: user },
+          { type: 'uint8', value: this.APP_ID }
+        ])
+
+      default:
+        return null
+    }
+  }
+}
