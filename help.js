9a5b0e9aaa0c76e90c99a1eed7fcf7289b36ff76
Zahra Namazi <namazi.q@gmail.com>
2021-12-21 13:46:07 +0330

    increase my allocation for test

--- help.js
+++ help.js
@@ -6,6 +6,6 @@ console.log(
   'To run Muon Node on your local run \n\n \t\t\t',
   '"npm run node -- -setup -n=2 -p=8080"\n \n\t\t\t',
   '-setup: if you want to change config \n\n \t\t\t',
-  '-n: number of env \n\n \t\t\t',
+  '-n: number of node \n\n \t\t\t',
   '-p: port\n \n'
 )
