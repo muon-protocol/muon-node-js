41dd0dee135e552e6c0437ca8d39d352679d9a97
mmd <mmd.mostafaee@gmail.com>
2022-02-01 15:11:29 +0330

    feat(synchronizer-oracle): Add timestamp to signatures url

--- apps/general/synchronizer-oracle.js
+++ apps/general/synchronizer-oracle.js
@@ -35,11 +35,13 @@ module.exports = {
             case 'signature':
                 let { tokenId, action, chain, useMultiplier } = params
 
+                const address = web3.utils.toChecksumAddress(tokenId);
+
                 const tokens = await axios
-                    .get(`${SYNCHRONIZER_SERVER}/${chain}/signatures.json`)
+                    .get(`${SYNCHRONIZER_SERVER}/${chain}/signatures.json?timestamp=${Date.now()}`)
                     .then(({ data }) => data)
 
-                if (!(tokenId in tokens)) {
+                if (!(address in tokens)) {
                     throw { message: 'Unknown token address' }
                 }
 
@@ -51,8 +53,7 @@ module.exports = {
                     useMultiplier: useMultiplier,
                     multiplier: multiplier,
                     price: token.price,
-                    fee: token.fee,
-                    address: web3.utils.toChecksumAddress(tokenId),
+                    address: address,
                     expireBlock: token.blockNo,
                     action: action,
                     chain: chain
@@ -70,7 +71,7 @@ module.exports = {
         } = request
         switch (method) {
             case 'signature': {
-                let { useMultiplier, multiplier, price, fee, address, expireBlock, action, chain } = result
+                let { useMultiplier, multiplier, price, address, expireBlock, action, chain } = result
 
                 let abi = []
 
@@ -81,14 +82,11 @@ module.exports = {
                 abi.push(...[
                     { type: 'address', value: String(address) },
                     { type: 'uint256', value: String(price) },
-                    { type: 'uint256', value: String(fee) },
                     { type: 'uint256', value: String(expireBlock) },
                     { type: 'uint256', value: String(ACTIONS[action]) },
                     { type: 'uint256', value: String(CHAINS[chain]) },
                     { type: 'uint8', value: this.APP_ID }
                 ])
-
-                console.log(abi);
 
                 return soliditySha3(abi)
             }
