352b2dd6147a5c239a682fdef0602141e7fe1851
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-02-06 04:10:02 +0330

    carebig

---
+++ apps/general/carebig.js
@@ -0,0 +1,62 @@
+const { axios, soliditySha3, floatToBN } = MuonAppUtils
+
+const APP_ID = 11
+
+const getTimestamp = () => Math.floor(Date.now() / 1000)
+
+function getRewards(address) {
+  return axios
+    .get(
+      'https://carebigtoken.io/api/public/rewards/'+address
+    )
+    .then(({ data }) => data)
+    .catch((err) => {
+      return err?.response?.data
+    })
+}
+
+module.exports = {
+  APP_NAME: 'carebig',
+
+  onRequest: async (request) => {
+    let {
+      method,
+      data: { params }
+    } = request
+    switch (method) {
+      case 'claim':
+        let { address } = params
+
+        let result = await getRewards(address);
+
+        if (!result.rewards || result.rewards == 0) {
+          throw { message: 'address not allowed for claim' }
+        }
+
+        return {
+          appId: APP_ID,
+          address,
+          rewards: floatToBN(result.rewards, 18).toString(10)
+        }
+
+      default:
+        throw { message: `Unknown method ${params}` }
+    }
+  },
+
+  hashRequestResult: (request, result) => {
+    let { method } = request
+    switch (method) {
+      case 'claim':
+        let { address } = result
+        return soliditySha3([
+          { type: 'uint256', value: APP_ID },
+          { type: 'address', value: address },
+          { type: 'uint256', value: request.data.result.rewards}
+        ])
+
+      default:
+        break
+    }
+  }
+}
