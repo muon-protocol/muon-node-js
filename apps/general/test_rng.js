560059fc33f1572da022a28a5b75417455a92f3b
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-02-17 03:08:06 +0330

    RNG

--- apps/general/test_rng.js
+++ apps/general/test_rng.js
@@ -2,8 +2,27 @@ const { axios, soliditySha3, floatToBN } = MuonAppUtils
 
 const APP_ID = 13
 
-function getRandomNumber(min, max){
-  return Math.random() * (max - min) + min;
+// Random seed function
+// More Info: 
+// https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
+Math.seed = function(s) {
+    var mask = 0xffffffff;
+    var m_w  = (123456789 + s) & mask;
+    var m_z  = (987654321 - s) & mask;
+
+    return function() {
+      m_z = (36969 * (m_z & 65535) + (m_z >>> 16)) & mask;
+      m_w = (18000 * (m_w & 65535) + (m_w >>> 16)) & mask;
+
+      var result = ((m_z << 16) + (m_w & 65535)) >>> 0;
+      result /= 4294967296;
+      return result;
+    }
+}
+
+getRandomNumber = function(seed){
+  var rngFunction = Math.seed(seed);
+  return rngFunction();
 }
 
 module.exports = {
@@ -18,7 +37,19 @@ module.exports = {
       case 'randint':
         let { address, min, max} = params
 
-        let randomNumber = getRandomNumber();
+        /*
+        We are using the timestamp as the seed.
+        To make it more secure, we can use another deterministic random number. For example:
+        - Solana’s block hash at timestamp+1 second
+        - BTC price at timestamp+1 second
+        …
+        */
+
+        let seed = request.data.timestamp;
+        
+        // all nodes will use the same seed and the random
+        // number will be the same on all nodes
+        let randomNumber = getRandomNumber(seed);
 
         return {
           appId: APP_ID,
@@ -32,7 +63,7 @@ module.exports = {
   },
 
   hashRequestResult: (request, result) => {
-    let { method } = request
+    let { method } = request;
     var number = result.number;
     if(request.data.result && request.data.result.number){
       number = request.data.result.number;
