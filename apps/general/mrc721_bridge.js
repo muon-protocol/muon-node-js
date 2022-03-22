4d218b25b1f19f815d85e1d313f62d2b94338cc6
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-03-22 23:26:12 +0430

    MRC721 updates

--- apps/general/mrc721_bridge.js
+++ apps/general/mrc721_bridge.js
@@ -10,12 +10,34 @@ const ABI_getTx = [
       { internalType: 'uint256', name: 'fromChain', type: 'uint256' },
       { internalType: 'uint256', name: 'toChain', type: 'uint256' },
       { internalType: 'address', name: 'user', type: 'address' },
+      { internalType: 'address', name: 'nftContract', type: 'address' },
+      { internalType: 'bool', name: 'transferParams', type: 'bool' },
       { internalType: 'uint256[]', name: 'nftId', type: 'uint256[]' }
     ],
     stateMutability: 'view',
     type: 'function'
   }
 ]
+
+ABI_encodeParams = [{
+      inputs: [
+        {
+          "internalType": "uint256[]",
+          "name": "ids",
+          "type": "uint256[]"
+        }
+      ],
+      name: "encodeParams",
+      outputs: [
+        {
+          "internalType": "bytes",
+          "name": "",
+          "type": "bytes"
+        }
+      ],
+      stateMutability: "view",
+      type: "function"
+    }]
 
 module.exports = {
   APP_NAME: 'mrc721_bridge',
@@ -29,7 +51,7 @@ module.exports = {
     switch (method) {
       case 'claim':
         let { depositAddress, depositTxId, depositNetwork } = params
-	//TODO: check chain
+        //TODO: check chain
         if (!depositAddress) throw { message: 'Invalid contarct address' }
         if (!depositTxId) throw { message: 'Invalid deposit Tx Id' }
         if (!depositNetwork) throw { message: 'Invalid deposit Network' }
@@ -40,6 +62,15 @@ module.exports = {
           ABI_getTx,
           depositNetwork
         )
+        result.nftParams = result.transferParams ? (
+          await ethCall(
+            result.nftContract,
+            "encodeParams",
+            [result.nftId],
+            ABI_encodeParams,
+            depositNetwork
+          )
+        ): '0x0';
         return result
 
       default:
@@ -57,7 +88,7 @@ module.exports = {
       case 'claim':
         let { depositAddress } = request.data.params
 
-        let { txId, tokenId, fromChain, toChain, user, nftId } = result
+        let { txId, tokenId, fromChain, toChain, user, nftId, nftParams } = result
 
         return soliditySha3([
           { type: 'uint32', value: this.APP_ID },
@@ -66,7 +97,8 @@ module.exports = {
           { type: 'uint256', value: fromChain },
           { type: 'uint256', value: toChain },
           { type: 'address', value: user },
-          { type: 'uint256[]', value: nftId }
+          { type: 'uint256[]', value: nftId },
+          { type: 'bytes', value: nftParams }
         ])
 
       default:
