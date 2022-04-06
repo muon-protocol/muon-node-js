f0968e5652ecf806ecda1bc03223e1df11fe4214
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-04-06 14:14:38 +0430

    dei_oracles_vwap_permissionless

--- apps/general/solidly_permissionless_oracles_vwap.js
+++ apps/general/solidly_permissionless_oracles_vwap.js
@@ -266,8 +266,8 @@ async function LPTokenPrice(token, pairs0, pairs1) {
       t1: item.callsReturnContext[0].returnValues[6]
     }))
     totalUSDA = (await tokenVWAP(metadata.t0, pairs0, pairs0Metadata)).mul(
-      new BN(metadata.r0)
-    )
+      reserveA
+    );
   }
 
   let totalUSDB = reserveB
@@ -285,8 +285,8 @@ async function LPTokenPrice(token, pairs0, pairs1) {
       t1: item.callsReturnContext[0].returnValues[6]
     }))
     totalUSDB = (await tokenVWAP(metadata.t1, pairs1, pairs1Metadata)).mul(
-      new BN(metadata.r1)
-    )
+      reserveB
+    );
   }
 
   let totalUSD = totalUSDA.add(totalUSDB)
