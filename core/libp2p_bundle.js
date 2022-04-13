d6fb7d337ce5c952cfc9767ef3b5ea9ef46ad96a
Sadegh Teymouri <sadeghte@gmail.com>
2022-04-13 08:55:10 +0430

    dependency update

--- core/libp2p_bundle.js
+++ core/libp2p_bundle.js
@@ -4,25 +4,20 @@ const WS = require('libp2p-websockets');
 const defaultsDeep = require('@nodeutils/defaults-deep')
 const Bootstrap = require('libp2p-bootstrap')
 const Multiplex = require('libp2p-mplex')
-import MulticastDNS from 'libp2p-mdns'
-const SECIO = require('libp2p-secio')
+// const SECIO = require('libp2p-secio')
+// import { NOISE } from '@chainsafe/libp2p-noise'
 const { NOISE } = require('libp2p-noise')
 const Gossipsub = require('libp2p-gossipsub')
 const KadDHT = require('libp2p-kad-dht')
 
 const DEFAULT_OPTS = {
-  dialer: {
-    maxParallelDials: 150, // 150 total parallel multiaddr dials
-    maxDialsPerPeer: 4, // Allow 4 multiaddrs to be dialed per peer in parallel
-    dialTimeout: 10e3 // 10 second dial timeout per peer dial
-  },
   modules: {
     transport: [
       TCP,
       WS,
     ],
     peerDiscovery: [
-      MulticastDNS
+      Bootstrap
     ],
     connEncryption: [
       NOISE,
@@ -35,34 +30,10 @@ const DEFAULT_OPTS = {
   },
   config: {
     peerDiscovery: {
-      autoDial: true,
-      [MulticastDNS.tag]: {
-        enabled: true
-      },
-      // Optimization
-      // Requiring bootstrap inline in components/libp2p to reduce the cli execution time
-      // [Bootstrap.tag] = 'bootstrap'
-      bootstrap: {
-        enabled: true
-      }
     },
     dht: {
       enabled: true
-    },
-    pubsub: {
-      enabled: true,
-      emitSelf: false
-    },
-    nat: {
-      enabled: true,
-      // description: `ipfs@${os.hostname()}`
     }
-  },
-  metrics: {
-    enabled: true
-  },
-  peerStore: {
-    persistence: true
   }
 }
 
