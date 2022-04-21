7798333f109f53b6afed4db7c0bd52bf48470eb9
Sadegh Teymouri <sadeghte@gmail.com>
2022-04-21 07:37:37 +0430

    bug fix

--- plugins/base/callable-plugin.js
+++ plugins/base/callable-plugin.js
@@ -2,14 +2,14 @@ const BasePlugin = require('./base-plugin')
 
 module.exports = class CallablePlugin extends BasePlugin {
 
-  remoteCall(peer, methodName, data){
+  remoteCall(peer, methodName, data, options){
     let remoteCall = this.muon.getPlugin('remote-call')
     let remoteMethodEndpoint = this.remoteMethodEndpoint(methodName)
     if(Array.isArray(peer)){
-      return Promise.all(peer.map(p => remoteCall.call(p, remoteMethodEndpoint, data)))
+      return Promise.all(peer.map(p => remoteCall.call(p, remoteMethodEndpoint, data, options)))
     }
     else{
-      return remoteCall.call(peer, remoteMethodEndpoint, data)
+      return remoteCall.call(peer, remoteMethodEndpoint, data, options)
     }
   }
 
