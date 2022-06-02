3cffc4219e3fa16473ef2d36325c31785588c86a
Zahra <namazi.q@gmail.com>
2022-06-02 11:34:57 +0430

    change help

--- help.js
+++ help.js
@@ -7,7 +7,7 @@ console.log(
   '"npm run devnet-init -- -setup -n=3 -p=8000"\n \n\t\t',
   emoji.get('large_blue_circle'),
   'To run Muon Node on your local run \n\n \t\t\t',
-  '"npm run devnet-run --  -n=3 -p=8000"\n \n\n\t\t',
+  '"npm run devnet-run -- -n=3 "\n \n\n\t\t',
   '-setup: if you want to change config \n\n \t\t',
   '-n: number of node  (default=2)\n\n \t\t',
   '-p: port (default=8000)\n \n'
