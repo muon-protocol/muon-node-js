53f413678342e10e8a51bebb7c35a71c5efe07a6
Sadegh Teymouri <sadeghte@gmail.com>
2022-09-05 12:14:09 +0430

    minor update

--- src/core/plugins/system-app.ts
+++ src/core/plugins/system-app.ts
@@ -18,7 +18,9 @@ class SystemApp extends CallablePlugin {
   @gatewayMethod('deploy')
   async __deployApp({params, callId}) {
     const availableNodes = this.TssPlugin.tssParty!.onlinePartners
-    const key = await this.TssPlugin.keyGen()
+    if(Object.keys(availableNodes).length < this.CollateralPlugin.TssThreshold)
+      throw "No enough partners to deploy app"
+    const key = await this.TssPlugin.keyGen(null, {maxPartners: Object.keys(availableNodes).length})
     return {
       available: availableNodes,
       data: {params, callId},
