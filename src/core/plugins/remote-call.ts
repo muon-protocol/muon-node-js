f33fb2a33af68c91510583054a08a3146b8a250a
Sadegh Teymouri <sadeghte@gmail.com>
2022-11-27 16:51:44 +0330

    log core remote method handling errors

--- src/core/plugins/remote-call.ts
+++ src/core/plugins/remote-call.ts
@@ -1,9 +1,15 @@
 import BasePlugin from'./base/base-plugin'
 const NetworkIpc = require('../../network/ipc')
+const errorLog = require('../../common/muon-log')('muon:core:plugins:remote-call:error')
 
 export default class RemoteCall extends BasePlugin {
   async handleCall(callId, method, params, callerInfo, responseStream){
-    return await this.emit(`${method}`, params, callerInfo)
+    try {
+      return await this.emit(`${method}`, params, callerInfo)
+    }catch (e) {
+      errorLog("error happened %o", e)
+      throw e
+    }
   }
 
   /**
