d88406dabde0ec44a177a94160a6d9f85f8fcb15
Sadegh Teymouri <sadeghte@gmail.com>
2022-12-16 23:39:47 +0330

    mpc dkg check by 4 partner

--- src/common/mpc/dkg.test.ts
+++ src/common/mpc/dkg.test.ts
@@ -18,13 +18,15 @@ const TssModule = require('../../utils/tss/index')
  */
 const N = TssModule.curve.n
 const t = 2;
-const NODE_1='1', NODE_2='2'
+const NODE_1='1', NODE_2='2', NODE_3='3', NODE_4='4'
 
 
 async function run() {
 
   const fakeNet1 = new FakeNetwork(NODE_1),
-    fakeNet2 = new FakeNetwork(NODE_2)
+    fakeNet2 = new FakeNetwork(NODE_2),
+    fakeNet3 = new FakeNetwork(NODE_3),
+    fakeNet4 = new FakeNetwork(NODE_4)
 
   for(let i=0 ; i<1000 ; i++) {
     const realPrivateKey = toBN(randomHex(32)).umod(N);
@@ -32,25 +34,30 @@ async function run() {
     /** DistributedKeyGen construction data */
     const cData = {
       id: 'dkg-1',
-      partners: [NODE_1, NODE_2],
+      partners: [NODE_1, NODE_2, NODE_3, NODE_4],
       t,
       pk: toBN(realPrivateKey)
     }
 
-    let [node1Result, node2Result] = await Promise.all([
+    let [node1Result, node2Result, node3Result, node4Result] = await Promise.all([
       /** run partner 1 */
       new DistributedKeyGeneration(cData.id, cData.partners, cData.t, cData.pk).process(fakeNet1),
       /** run partner 2 */
       new DistributedKeyGeneration(cData.id, cData.partners, cData.t, cData.pk).process(fakeNet2),
-      // MpcRunner.process(DistributedKeyGeneration, constructionData, new FakeNetwork(NODE_2)),
+      /** run partner 2 */
+      new DistributedKeyGeneration(cData.id, cData.partners, cData.t, cData.pk).process(fakeNet3),
+      /** run partner 2 */
+      new DistributedKeyGeneration(cData.id, cData.partners, cData.t, cData.pk).process(fakeNet4),
     ]);
 
     const shares = [
       {i: 1, key: TssModule.keyFromPrivate(node1Result)},
       {i: 2, key: TssModule.keyFromPrivate(node2Result)},
+      {i: 3, key: TssModule.keyFromPrivate(node3Result)},
+      {i: 4, key: TssModule.keyFromPrivate(node4Result)},
     ]
     const reconstructedKey = bn2str(TssModule.reconstructKey(shares, t, 0))
-    const keyShouldToBe = bn2str(toBN(realPrivateKey).muln(2).umod(N))
+    const keyShouldToBe = bn2str(toBN(realPrivateKey).muln(4).umod(N))
 
     if(reconstructedKey === keyShouldToBe)
       console.log(`i: ${i}, match: OK`)
