b1c37dc8bb0b5ff4d6bf2fe3a343365a37c45aff
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-12-22 20:08:30 +0330

    upgraded libp2p

--- src/network/index.ts
+++ src/network/index.ts
@@ -1,46 +1,46 @@
 import NetworkContentPlugin from "./plugins/content-plugin";
-let mongoose = require('mongoose')
-const Events = require('events-async');
-const Libp2pBundle = require('./libp2p_bundle')
-const loadConfigs = require('./configurations')
-const PeerId = require('peer-id')
-const chalk = require('chalk')
-const emoji = require('node-emoji')
-const isPrivate = require('libp2p-utils/src/multiaddr/is-private')
-const coreIpc = require('../core/ipc')
-const { MessagePublisher } = require('../common/message-bus')
-const log = require('../common/muon-log')('muon:network')
+let mongoose = require("mongoose");
+const Events = require("events-async");
+import { create, bootstrap } from "./libp2p_bundle";
+const loadConfigs = require("./configurations");
+const PeerId = require("peer-id");
+const chalk = require("chalk");
+const emoji = require("node-emoji");
+const isPrivate = require("libp2p-utils/src/multiaddr/is-private");
+const coreIpc = require("../core/ipc");
+const { MessagePublisher } = require("../common/message-bus");
+const log = require("../common/muon-log")("muon:network");
 
-import CollateralPlugin from './plugins/collateral-info';
-import IpcHandlerPlugin from './plugins/network-ipc-handler'
-import IpcPlugin from './plugins/network-ipc-plugin'
-import RemoteCallPlugin from './plugins/remote-call'
-import NetworkBroadcastPlugin from './plugins/network-broadcast'
+import CollateralPlugin from "./plugins/collateral-info";
+import IpcHandlerPlugin from "./plugins/network-ipc-handler";
+import IpcPlugin from "./plugins/network-ipc-plugin";
+import RemoteCallPlugin from "./plugins/remote-call";
+import NetworkBroadcastPlugin from "./plugins/network-broadcast";
 
 class Network extends Events {
-  configs
-  libp2p
-  _plugins = {}
-  private connectedPeers: {[index: string]: boolean} = {}
+  configs;
+  libp2p;
+  _plugins = {};
+  private connectedPeers: { [index: string]: boolean } = {};
 
   constructor(configs) {
-    super()
+    super();
     this.configs = configs;
   }
 
-  getConnectedPeers(): {[index: string]: boolean} {
+  getConnectedPeers(): { [index: string]: boolean } {
     return this.connectedPeers;
   }
 
   async _initializeLibp2p() {
-    log(`libp2p initializing ...`)
+    log(`libp2p initializing ...`);
     const configs = this.configs.libp2p;
-    let peerId = await PeerId.createFromJSON(configs.peerId)
-    let announceFilter = (multiaddrs) => multiaddrs.filter(m => !isPrivate(m));
-    if(process.env.DISABLE_ANNOUNCE_FILTER)
-      announceFilter = mas => mas
+    let peerId = await PeerId.createFromJSON(configs.peerId);
+    let announceFilter = (multiaddrs) =>
+      multiaddrs.filter((m) => !isPrivate(m));
+    if (process.env.DISABLE_ANNOUNCE_FILTER) announceFilter = (mas) => mas;
 
-    const libp2p = await Libp2pBundle.create({
+    const libp2p = await create({
       peerId,
       addresses: {
         listen: [
@@ -48,67 +48,75 @@ class Network extends Events {
           // `/ip4/${configs.host}/tcp/${configs.port}/p2p/${process.env.PEER_ID}`,
           // `/ip4/0.0.0.0/tcp/${parseInt(configs.port)+1}/ws`,
         ],
-        announceFilter
+        announceFilter,
       },
       config: {
         peerDiscovery: {
-          [Libp2pBundle.Bootstrap.tag]: {
-            list: [...configs.bootstrap],
-            interval: 5000, // default is 10 ms,
-            enabled: configs.bootstrap.length > 0
-          }
-        }
-      }
+          // [Libp2pBundle.Bootstrap.tag]: {
+          //   list: [...configs.bootstrap],
+          //   interval: 5000, // default is 10 ms,
+          //   enabled: configs.bootstrap.length > 0,
+          // },
+        },
+      },
     });
-    libp2p.connectionManager.on('peer:connect', this.onPeerConnect.bind(this))
-    libp2p.connectionManager.on('peer:disconnect', this.onPeerDisconnect.bind(this))
-    libp2p.on('peer:discovery', this.onPeerDiscovery.bind(this))
+    libp2p.connectionManager.addEventListener("peer:connect", this.onPeerConnect.bind(this));
+    libp2p.connectionManager.addEventListener(
+      "peer:disconnect",
+      this.onPeerDisconnect.bind(this)
+    );
+    libp2p.addEventListener("peer:discovery", this.onPeerDiscovery.bind(this));
 
-    this.peerId = peerId
-    this.libp2p = libp2p
+    this.peerId = peerId;
+    this.libp2p = libp2p;
   }
 
   async _initializePlugin() {
-    const { plugins } = this.configs
+    const { plugins } = this.configs;
     for (let pluginName in plugins) {
-      const [plugin, configs] = plugins[pluginName]
+      const [plugin, configs] = plugins[pluginName];
       this._plugins[pluginName] = new plugin(this, configs);
       await this._plugins[pluginName].onInit();
     }
-    log('plugins initialized.')
+    log("plugins initialized.");
   }
 
   getPlugin(pluginName) {
-    return this._plugins[pluginName]
+    return this._plugins[pluginName];
   }
 
   async start() {
-    log(`libp2p starting peerId: ${this.peerId.toB58String()} ...`)
-    await this.libp2p.start()
+    log(`libp2p starting peerId: ${this.peerId.toB58String()} ...`);
+    await this.libp2p.start();
 
     if (this.configs.libp2p.natIp) {
-      let {port, natIp} = this.configs.libp2p
-      this.libp2p.addressManager.addObservedAddr(`/ip4/${natIp}/tcp/${port}/p2p/${this.peerId.toB58String()}`);
+      let { port, natIp } = this.configs.libp2p;
+      this.libp2p.addressManager.addObservedAddr(
+        `/ip4/${natIp}/tcp/${port}/p2p/${this.peerId.toB58String()}`
+      );
     }
 
     log(
-      emoji.get('moon') + " " +
-      chalk.blue(' Node ready ') + " " +
-      emoji.get('headphones') + " " +
-      chalk.blue(` Listening on: ${this.configs.libp2p.port}`)
-    )
+      emoji.get("moon") +
+        " " +
+        chalk.blue(" Node ready ") +
+        " " +
+        emoji.get("headphones") +
+        " " +
+        chalk.blue(` Listening on: ${this.configs.libp2p.port}`)
+    );
 
     // if(process.env.VERBOSE) {
-    log("====================== Bindings ====================")
+    log("====================== Bindings ====================");
     this.libp2p.multiaddrs.forEach((ma) => {
-      log(ma.toString())
+      log(ma.toString());
       // console.log(`${ma.toString()}/p2p/${this.libp2p.peerId.toB58String()}`)
-    })
-    log("====================================================")
+    });
+    log("====================================================");
     // }
 
     // if (this.libp2p.isStarted()) {
-      this._onceStarted();
+    this._onceStarted();
     // } else {
     //   // this.libp2p.once('start', this._onceStarted.bind(this))
     //   this.libp2p.addEventListener('start', this._onceStarted.bind(this))
@@ -116,136 +124,147 @@ class Network extends Events {
   }
 
   async _onceStarted() {
-    log(`muon started at ${new Date()} (node-js version ${process.versions.node}).`)
+    log(
+      `muon started at ${new Date()} (node-js version ${
+        process.versions.node
+      }).`
+    );
     for (let pluginName in this._plugins) {
-      this._plugins[pluginName].onStart()
-        .catch(e => {
-          console.error(`network: plugins start error`, e)
-        })
+      this._plugins[pluginName].onStart().catch((e) => {
+        console.error(`network: plugins start error`, e);
+      });
     }
   }
 
   onPeerConnect(connection) {
     log(
-      emoji.get('moon') + " " +
-      chalk.blue(' Node connected to ') + " " +
-      emoji.get('large_blue_circle') + " " +
-      chalk.blue(` ${connection.remotePeer.toB58String()}`)
-    )
-    this.connectedPeers[connection.remotePeer._idB58String] = true
-    this.emit('peer:connect', connection.remotePeer)
-    coreIpc.fireEvent({type: "peer:connect", data: connection.remotePeer.toB58String()})
+      emoji.get("moon") +
+        " " +
+        chalk.blue(" Node connected to ") +
+        " " +
+        emoji.get("large_blue_circle") +
+        " " +
+        chalk.blue(` ${connection.remotePeer.toB58String()}`)
+    );
+    this.connectedPeers[connection.remotePeer._idB58String] = true;
+    this.emit("peer:connect", connection.remotePeer);
+    coreIpc.fireEvent({
+      type: "peer:connect",
+      data: connection.remotePeer.toB58String(),
+    });
   }
 
   onPeerDisconnect(connection) {
-    delete this.connectedPeers[connection.remotePeer._idB58String]
+    delete this.connectedPeers[connection.remotePeer._idB58String];
     log(
-      emoji.get('moon') + " " +
-      chalk.red(' Node disconnected') + " " +
-      emoji.get('large_blue_circle') + " " +
-      chalk.red(` ${connection.remotePeer.toB58String()}`)
+      emoji.get("moon") +
+        " " +
+        chalk.red(" Node disconnected") +
+        " " +
+        emoji.get("large_blue_circle") +
+        " " +
+        chalk.red(` ${connection.remotePeer.toB58String()}`)
     );
-    this.emit('peer:disconnect', connection.remotePeer)
-    coreIpc.fireEvent({type: "peer:disconnect", data: connection.remotePeer.toB58String()})
+    this.emit("peer:disconnect", connection.remotePeer);
+    coreIpc.fireEvent({
+      type: "peer:disconnect",
+      data: connection.remotePeer.toB58String(),
+    });
   }
 
   async onPeerDiscovery(peerId) {
-    this.connectedPeers[peerId._idB58String] = true
-    this.emit('peer:discovery', peerId)
-    coreIpc.fireEvent({type: "peer:discovery", data: peerId.toB58String()})
-    log('found peer');
+    this.connectedPeers[peerId._idB58String] = true;
+    this.emit("peer:discovery", peerId);
+    coreIpc.fireEvent({ type: "peer:discovery", data: peerId.toB58String() });
+    log("found peer");
     try {
-      const peerInfo = await this.libp2p.peerRouting.findPeer(peerId)
+      const peerInfo = await this.libp2p.peerRouting.findPeer(peerId);
       console.log({
         peerId: peerId.toB58String(),
         multiaddrs: peerInfo.multiaddrs,
         // peerInfo,
-      })
+      });
     } catch (e) {
-      console.log('Error Muon.onPeerDiscovery', e);
+      console.log("Error Muon.onPeerDiscovery", e);
     }
   }
 }
 
-function getLibp2pBootstraps(){
+function getLibp2pBootstraps() {
   return Object.keys(process.env)
-    .filter(key => key.startsWith('PEER_BOOTSTRAP_'))
-    .map(key => process.env[key])
-      .filter(val => val!= undefined);
+    .filter((key) => key.startsWith("PEER_BOOTSTRAP_"))
+    .map((key) => process.env[key])
+    .filter((val) => val != undefined);
 }
 
-function clearMessageBus(){
-  let mp = new MessagePublisher("temp")
+function clearMessageBus() {
+  let mp = new MessagePublisher("temp");
   const redis = mp.sendRedis;
   return new Promise((resolve, reject) => {
     redis.keys(`${mp.channelPrefix}*`, function(err, rows) {
-      if(err)
-        return reject(err);
-      for(var i = 0, j = rows.length; i < j; ++i) {
-        redis.del(rows[i])
+      if (err) return reject(err);
+      for (var i = 0, j = rows.length; i < j; ++i) {
+        redis.del(rows[i]);
       }
       resolve(true);
     });
-  })
+  });
 }
 
 async function start() {
-  log("connecting to mongodb ...")
+  log("connecting to mongodb ...");
   await mongoose.connect(process.env.MONGODB_CS, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+    useUnifiedTopology: true,
+  });
 
-  if (!mongoose.connection)
-    throw 'Error connecting to MongoDB'
+  if (!mongoose.connection) throw "Error connecting to MongoDB";
 
-  log(`MongoDB successfully connected.`)
+  log(`MongoDB successfully connected.`);
 
-  log("starting ...")
+  log("starting ...");
   await clearMessageBus();
 
-  let {
-    net,
-    tss,
-  } = await loadConfigs();
+  let { net, tss } = await loadConfigs();
 
-  if(!process.env.PEER_PORT){
-    throw {message: "peer listening port should be defined in .env file"}
+  if (!process.env.PEER_PORT) {
+    throw { message: "peer listening port should be defined in .env file" };
   }
-  if(!process.env.PEER_ID || !process.env.PEER_PUBLIC_KEY || !process.env.PEER_PRIVATE_KEY){
-    throw {message: "peerId info should be defined in .env file"}
+  if (
+    !process.env.PEER_ID ||
+    !process.env.PEER_PUBLIC_KEY ||
+    !process.env.PEER_PRIVATE_KEY
+  ) {
+    throw { message: "peerId info should be defined in .env file" };
   }
   let configs = {
     libp2p: {
       peerId: {
         id: process.env.PEER_ID,
         pubKey: process.env.PEER_PUBLIC_KEY,
-        privKey: process.env.PEER_PRIVATE_KEY
+        privKey: process.env.PEER_PRIVATE_KEY,
       },
       natIp: process.env.PEER_NAT_IP,
       host: process.env.PEER_HOST || "0.0.0.0",
       port: process.env.PEER_PORT,
-      bootstrap: getLibp2pBootstraps()
+      bootstrap: getLibp2pBootstraps(),
     },
     plugins: {
-      'collateral': [CollateralPlugin, {}],
-      'broadcast': [NetworkBroadcastPlugin, {}],
-      'content': [NetworkContentPlugin, {}],
-      'remote-call': [RemoteCallPlugin, {}],
-      'ipc': [IpcPlugin, {}],
-      'ipc-handler': [IpcHandlerPlugin, {}],
+      collateral: [CollateralPlugin, {}],
+      broadcast: [NetworkBroadcastPlugin, {}],
+      content: [NetworkContentPlugin, {}],
+      "remote-call": [RemoteCallPlugin, {}],
+      ipc: [IpcPlugin, {}],
+      "ipc-handler": [IpcHandlerPlugin, {}],
     },
     net,
     // TODO: pass it into the tss-plugin
-    tss
+    tss,
   };
   const network = new Network(configs);
-  await network._initializeLibp2p()
-  await network._initializePlugin()
+  await network._initializeLibp2p();
+  await network._initializePlugin();
   await network.start();
 }
 
-export {
-  Network,
-  start
-}
+export { Network, start };
