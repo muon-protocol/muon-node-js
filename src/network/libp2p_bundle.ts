2a4075a109c407cf9403104e324a31c62cdb66d9
Sadegh Teymouri <sadeghte@gmail.com>
2023-01-21 18:07:28 +0330

    debug

--- src/network/libp2p_bundle.ts
+++ src/network/libp2p_bundle.ts
@@ -14,8 +14,11 @@ const DEFAULT_OPTS = {
   // TODO: move path to env
   datastore: new LevelDatastore(`./muon-data/${process.env.SIGN_WALLET_ADDRESS!.substr(-20)}/`),
   transports: [
-    tcp(),
-    // webSockets()
+    tcp({
+      outboundSocketInactivityTimeout: 0,
+      inboundSocketInactivityTimeout: 0
+    }),
+    webSockets()
   ],
   connectionEncryption: [
     noise(),
