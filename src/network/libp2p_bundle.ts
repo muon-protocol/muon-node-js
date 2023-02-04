dcaf7188de4c22c06e703fb6e208669a613d23dc
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2023-02-05 00:09:07 +0330

    limit dht nodes

--- src/network/libp2p_bundle.ts
+++ src/network/libp2p_bundle.ts
@@ -12,7 +12,7 @@ import { LevelDatastore } from "datastore-level";
 
 const DEFAULT_OPTS = {
   // TODO: move path to env
-  datastore: new LevelDatastore(`./muon-data/${process.env.SIGN_WALLET_ADDRESS!.substr(-20)}/`),
+  //datastore: new LevelDatastore(`./muon-data/${process.env.SIGN_WALLET_ADDRESS!.substr(-20)}/`),
   transports: [
     tcp({
       // default timeout is 5 mins and nodes disconnect
