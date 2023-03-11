85b601b8326cbd2fbb324012ef69e6dd7541d8aa
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2023-03-12 00:45:44 +0330

    bugfix: auto-update issues

--- scripts/auto-update.sh
+++ scripts/auto-update.sh
@@ -35,7 +35,6 @@ check_for_update (){
 
     # restart services
     if [[ -z $_NODE ]]
-        # uses env _NODE by default
     then
         _NODE=`which node`;
     fi
@@ -47,12 +46,9 @@ check_for_update (){
         _NPM=/usr/local/bin/npm
     fi
 
-    #log `$_NODE  $_NPM install`
-
     git checkout package.json package-lock.json
     if git pull --recurse-submodules origin "$current_branch" | grep -q 'Already up to date'; then
-#        log "Node: [`which node`]    PM2: [`which pm2`]";
-        ``;
+        echo "No new updates";        
     else
         log "========== updating detected ===========";
         log "Installing dependencies: $_NODE  $_NPM install";
