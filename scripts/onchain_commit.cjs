68d4dcce8a15e63ae9dfbe580df71c9d22b63bfb
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2023-03-18 21:13:51 +0330

    onchain deploy scripts

--- scripts/onchain_commit.cjs
+++ scripts/onchain_commit.cjs
@@ -4,6 +4,8 @@ const nodeManagerContractAddress = netConf.nodeManager.address;
 const nodeManagerABI = require('../src/data/NodeManager-ABI.json');
 
 const Web3 = require("web3")
+
+//TODO: load the correct provider
 const web3 = new Web3(process.env.WEB3_PROVIDER_BSCTEST);
 
 const nodeManagerContract = new web3.eth.Contract(
