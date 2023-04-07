b8420f7229f56a8502a18305ae1baf23c77899ac
Sadegh Teymouri <sadeghte@gmail.com>
2023-04-07 15:46:16 +0330

    admin protect urls

--- src/gateway/middlewares.ts
+++ src/gateway/middlewares.ts
@@ -1,3 +1,11 @@
+import {muonSha3} from "../utils/sha3.js";
+import * as crypto from "../utils/crypto.js";
+
+/**  */
+const ADMIN_WALLETS = (`${process.env.ADMIN_WALLETS}${process.env.SIGN_WALLET_ADDRESS}`)
+  .split("|")
+  .filter(w => !!w)
+  .map(w => w.toLowerCase())
 
 export const mixGetPost = (req, res, next) => {
   // @ts-ignore
@@ -6,4 +14,33 @@ export const mixGetPost = (req, res, next) => {
     ...req.body,
   }
   next();
+}
+
+export const onlyAdmins = (req, res, next) => {
+  // @ts-ignore
+  const {at} = req.mixed
+  if(at) {
+    let [timestamp, lifetime, signature] = at.split(':')
+    timestamp = parseInt(timestamp)
+    if(timestamp < 0)
+      throw `bad access token`
+    let hash = muonSha3(
+      {t: 'uint64', v: timestamp},
+      {t: 'uint64', v: lifetime},
+      {t: 'string', v: 'muon-admin-access'},
+    )
+
+    const signer = crypto.recover(hash, signature)
+
+    if(!ADMIN_WALLETS.includes(signer.toLowerCase()))
+      throw `Admin restricted`
+
+    if(timestamp + lifetime < Date.now())
+      throw `access token expired`
+
+    next();
+  }
+  else {
+    throw `Admin restricted`
+  }
 }
