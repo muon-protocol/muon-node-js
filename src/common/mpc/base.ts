f8de14a2e40c031bcab88f79e7bbb6ea5b3ba290
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2023-06-20 02:09:38 +0330

    added more logs

--- src/common/mpc/base.ts
+++ src/common/mpc/base.ts
@@ -268,7 +268,7 @@ export class MultiPartyComputation {
         );
 
         if(qualifiedPartners.length < this.t) {
-          throw `${this.ConstructorName} needs ${this.t} partners but only [${qualifiedPartners.join(',')}] are qualified. partners=[${this.partners.join(',')}], round=${currentRound}, partyErrors=${partyErrors}`
+          throw `${this.ConstructorName} needs ${this.t} partners but only [${qualifiedPartners.join(',')}] are qualified. partners=[${this.partners.join(',')}], round=${currentRound}, partyErrors=${JSON.stringify(partyErrors)}`
         }
       }
 
