223365e98579dc5ccf32464cba7f07975395b927
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2023-07-18 21:47:05 +0330

    debug reshare

--- src/built-in-apps/app-deployment.js
+++ src/built-in-apps/app-deployment.js
@@ -448,8 +448,8 @@ module.exports = {
                 }
 
                 let difference = symmetricDifference(selectedNodes, selectedNodes2).length / selectedNodes2.length
-                if(difference > NODES_SELECTION_TOLERANCE)
-                    throw `selected nodes mismatched.`
+                // if(difference > NODES_SELECTION_TOLERANCE)
+                //     throw `selected nodes mismatched.`
 
                 const pendingPeriod = !!pending ? pending : tssConfigs.pendingPeriod
 
@@ -501,8 +501,8 @@ module.exports = {
                 selectedNodes2 = lodash.uniq([...nodesToKeep, ...selectedNodes2]).slice(0, n);
 
                 let difference = symmetricDifference(selectedNodes, selectedNodes2).length / selectedNodes2.length
-                if(difference > NODES_SELECTION_TOLERANCE)
-                    throw `selected nodes mismatched.`
+                // if(difference > NODES_SELECTION_TOLERANCE)
+                //     throw `selected nodes mismatched.`
 
                 const ttl = !!userDefinedTTL ? userDefinedTTL : prevContext.ttl;
                 const pendingPeriod = !!pending ? pending : prevContext.pendingPeriod;
