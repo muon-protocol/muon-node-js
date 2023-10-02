f47d6226c89a5971ce4445b9eb7d58b10bc84f7c
farazfo@yahoo.com <11iloveu005>
2023-10-02 10:30:41 +0330

    sex max heap in ecosystem.config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,12 @@
 module.exports = {
   apps : [{
     "name": "muon-node-js-alice2",
-    "script": "npm",
-    "args" : "start",
+    "script": "NODE_OPTIONS=--max-old-space-size=3072 npm start",
     "watch"  : false,
     "ignore_watch": ["node_modules"],
     "log_date_format" : "YYYY-MM-DD HH:mm",
     "autorestart": false,
     "max_memory_restart": "3G",
-    "node_args": "--max_old_space_size=3072 --max-old_space_size=3072",
-
     "env": {
       "NODE_ENV": "production"
     },
